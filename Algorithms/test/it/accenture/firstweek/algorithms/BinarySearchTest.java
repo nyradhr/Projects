efd6b216be7540701050f1915585fd914228c6b5
nyradhr <alessio.piccione@hotmail.it>
2022-06-23 13:22:36 +0200

    fixed tests on BinarySearchTest

--- Algorithms/test/it/accenture/firstweek/algorithms/BinarySearchTest.java
+++ Algorithms/test/it/accenture/firstweek/algorithms/BinarySearchTest.java
@@ -1,9 +1,6 @@
 package it.accenture.firstweek.algorithms;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-import java.lang.reflect.Array;
+import org.junit.jupiter.api.*;
 
 public class BinarySearchTest {
 
@@ -11,6 +8,7 @@ public class BinarySearchTest {
 
     @Test
     public void binarySearch_returns_position_when_target_present() {
+        System.out.println("binarySearch_returns_position_when_target_present");
         double target = 7.0;
         int pos = ArrayAlgorithms.binarySearch(target,a1);
         Assertions.assertEquals(3, pos);
@@ -18,6 +16,7 @@ public class BinarySearchTest {
 
     @Test
     public void binarySearch_returns_minus_1_when_target_outside_right_range() {
+        System.out.println("binarySearch_returns_minus_1_when_target_outside_right_range");
         double target = 15.0;
         int pos = ArrayAlgorithms.binarySearch(target, a1);
         Assertions.assertEquals(-1, pos);
@@ -25,6 +24,7 @@ public class BinarySearchTest {
 
     @Test
     public void binarySearch_returns_minus_1_when_target_outside_left_range(){
+        System.out.println("binarySearch_returns_minus_1_when_target_outside_left_range");
         double target = 1.0;
         int pos = ArrayAlgorithms.binarySearch(target, a1);
         Assertions.assertEquals(-1, pos);
@@ -32,6 +32,7 @@ public class BinarySearchTest {
 
     @Test
     public void binarySearch_returns_minus_1_when_target_in_range_but_not_found() {
+        System.out.println("binarySearch_returns_minus_1_when_target_in_range_but_not_found");
         double target = 6.0;
         int pos = ArrayAlgorithms.binarySearch(target,a1);
         Assertions.assertEquals(-1, pos);
@@ -39,6 +40,7 @@ public class BinarySearchTest {
 
     @Test //fans condition first num
     public void binarySearchTest_returns_position_for_first_element() {
+        System.out.println("binarySearchTest_returns_position_for_first_element");
         double target = 2.0;
         int pos = ArrayAlgorithms.binarySearch(target, a1);
         Assertions.assertEquals(0, pos);
@@ -46,9 +48,30 @@ public class BinarySearchTest {
 
     @Test //fans condition first num
     public void binarySearchTest_returns_position_for_last_element() {
+        System.out.println("binarySearchTest_returns_position_for_last_element");
         double target = 10.0;
         int pos = ArrayAlgorithms.binarySearch(target, a1);
         Assertions.assertEquals(4, pos);
+    }
+
+    @BeforeAll
+    public static void initClass() {
+        System.out.println("initClass");
+    }
+
+    @AfterAll //possible usage: tear down
+    public static void endClass() {
+        System.out.println("endClass");
+    }
+
+    @BeforeEach
+    public void startTest() {
+        System.out.println("startTest");
+    }
+
+    @AfterEach
+    public void endTest() {
+        System.out.println("endTest");
     }
 
 }
